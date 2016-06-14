28490cd471433e48c2df7cdfd75795e8acd39414
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2016-06-15 01:53:06 +0200

    binner_default_filler

--- src/binner.hh
+++ src/binner.hh
@@ -1,5 +1,5 @@
-#ifndef binner_h
-#define binner_h
+#ifndef binner_hh
+#define binner_hh
 
 #include <limits>
 #include <algorithm>
@@ -13,11 +13,37 @@
  * bin = nbins+1; overflow bin
  */
 
-template <typename Bin, typename Edge = double>
+template <bool Cond, class Type = void>
+using enable_t = typename std::enable_if<Cond,Type>::type;
+
+template <typename Bin, typename Enable = void>
+struct binner_default_filler {
+  template <typename... TT>
+  inline enable_t<(sizeof...(TT) > 1)>
+  // TODO: forward template arguments
+  operator()(Bin& bin, TT... args) noexcept(noexcept(bin(args...)))
+  { bin(args...); }
+};
+
+template <typename Bin>
+struct binner_default_filler<Bin, enable_t<std::is_arithmetic<Bin>::value>> {
+  template <typename T>
+  // TODO: forward template argument
+  inline void operator()(Bin& bin, T x) noexcept { bin += x; }
+};
+
+// TODO: overload for += operator with 1 argument
+// http://stackoverflow.com/questions/36296301/c-sfinae-detect-increment-operator
+
+//===================================================================
+
+template <typename Bin, typename Edge = double,
+          typename Filler = binner_default_filler<Bin>>
 class binner {
 public:
-  typedef Bin   bin_t;
-  typedef Edge edge_t;
+  typedef Bin    bin_t;
+  typedef Edge   edge_t;
+  typedef Filler filler_t;
   typedef typename std::vector<edge_t>::iterator  edge_iter;
   typedef typename std::vector< bin_t>::iterator   bin_iter;
   typedef typename std::vector<edge_t>::size_type size_type;
@@ -25,18 +51,6 @@ public:
 protected:
   std::vector<edge_t> _edges;
   std::vector< bin_t> _bins;
-
-  template <bool Cond, class Type = void>
-  using enable_if_t = typename std::enable_if<Cond,Type>::type;
-
-  template <typename T1, typename T2>
-  inline enable_if_t<std::is_arithmetic<T1>::value> _fill(T1& x, T2 y) noexcept
-  { x += y; }
-
-  template <typename T1, typename... TT>
-  inline enable_if_t< !std::is_arithmetic<T1>::value || (sizeof...(TT) > 1) >
-  _fill(T1& x, TT... args) noexcept(noexcept(x(args...)))
-  { x(args...); }
 
 public:
   binner() { }
@@ -65,7 +79,9 @@ public:
     _bins.resize(_edges.size()+1);
   }
 
-  size_type fill(edge_t e) {
+  //---------------------------------------------
+
+  size_type find_bin_index(edge_t e) noexcept {
     size_type i = _edges.size()-1;
     for (;;--i) {
       if (e >= _edges[i]) {
@@ -74,28 +90,33 @@ public:
       }
       if (i==0) break;
     }
-    _bins[i]++;
+    return i;
+  }
+
+  size_type fill(edge_t e) {
+    size_type i = find_bin_index(e);
+    ++_bins[i];
     return i;
   }
 
   template <typename... TT>
   size_type fill(edge_t e, const TT&... xx) {
-    size_type i = _edges.size()-1;
-    for (;;--i) {
-      if (e >= _edges[i]) {
-        ++i;
-        break;
-      }
-      if (i==0) break;
-    }
-    _fill(_bins[i], xx...);
+    size_type i = find_bin_index(e);
+    filler_t()(_bins[i], xx...);
     return i;
   }
 
+  //---------------------------------------------
+
+  template <typename... TT>
+  void fill_bin(size_type i) { ++_bins.at(i); }
+
   template <typename... TT>
   void fill_bin(size_type i, const TT&... xx) {
-    _fill(_bins.at(i), xx...);
+    filler_t()(_bins.at(i), xx...);
   }
+
+  //---------------------------------------------
 
   inline bin_t& operator[](size_type i) noexcept {
     return _bins[i];
@@ -111,6 +132,8 @@ public:
     return _bins.at(i);
   }
 
+  //---------------------------------------------
+
   edge_t ledge(size_type i) const {
     return i ? _edges.at(i-1) : -std::numeric_limits<edge_t>::infinity();
   }
@@ -118,6 +141,8 @@ public:
     return i<_edges.size() ? _edges.at(i)
                            : std::numeric_limits<edge_t>::infinity();
   }
+
+  //---------------------------------------------
 
   size_type nbins() const { return _edges.size()-1; }
 
